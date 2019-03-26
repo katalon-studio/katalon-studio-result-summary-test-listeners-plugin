281a2b09d44a051ce10d219c40f10297aef68610
Alex Smith <dev.alex.88@gmail.com>
2019-03-26 16:46:44 +0700

    initial commit

---
+++ Test Suites/Test Suite.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>bc7feec8-255d-4c10-b819-a6c6b320617e</testSuiteGuid>
+   <testCaseLink>
+      <guid>f0fb3bf2-2875-463f-b4bb-fb4fe99a33d8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c16db733-4703-4c96-b742-178fa9c61532</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
